c8a36292ccac4bc0e7e43a4649924d4d5a9ad48d
Ankoor <ankoor.apte@gmail.com>
2020-05-25 11:13:35 -0700

    revamped sidebar

--- app.js
+++ app.js
@@ -6,26 +6,36 @@ var app = new Vue({
   template: `
   <b-container>
     <b-row>
-      <b-button variant="outline-dark" v-b-toggle.sidebar-1><b-icon-list></b-icon-list>Menu</b-button>
-      <b-sidebar id="sidebar-1" title="Ankoor" shadow>
-        <p> Hello </p>
-      </b-sidebar>
-      <b-input-group prepend="Enter password" class="mt-3">
-        <b-form-input v-model="password"></b-form-input>
-        <b-input-group-append>
-          <b-button @click="unlock" variant="info">Unlock</b-button>
-        </b-input-group-append>
-      </b-input-group>
-    </b-row>
-    <b-row v-if="auth">
-      <test-component></test-component>
+      <b-col>
+        <b-button variant="outline-dark" v-b-toggle.sidebar-1><b-icon-list></b-icon-list>Menu</b-button>
+        <b-sidebar id="sidebar-1" title="Ankoor" shadow backdrop-variant="dark" backdrop>
+        <b-list-group flush>
+          <b-list-group-item @click="menu = 1">me</b-list-group-item>
+          <b-list-group-item @click="menu = 2">resumé</b-list-group-item>
+          <b-list-group-item @click="menu = 3">news</b-list-group-item>
+          <b-list-group-item @click="menu = 4">my stuff</b-list-group-item>
+        </b-list-group>
+        </b-sidebar>
+        <b-collapse v-if="menu === 4">
+          <b-input-group>
+            <b-form-input v-model="password"></b-form-input>
+            <b-input-group-append>
+              <b-button @click="unlock" variant="info">Unlock</b-button>
+            </b-input-group-append>
+          </b-input-group>
+          <b-row v-if="auth">
+            <test-component></test-component>
+          </b-row>
+        </b-collapse>
+      </b-col>
     </b-row>
   </b-container>
   `,
   data() {
     return {
       password: '',
-      auth: false
+      auth: false,
+      menu: 0,
     }
   },
   methods: {
