f5fc424160f16bb3264db3dba7e80dc4a462c10d
Ankoor <ankoor.apte@gmail.com>
2020-05-25 00:05:20 -0700

    revamping stuff

--- app.js
+++ app.js
@@ -6,22 +6,34 @@ var app = new Vue({
   template: `
   <b-container>
     <b-row>
-      <b-button @click="unlock">Unlock</b-button>
+      <b-input-group prepend="Enter password" class="mt-3">
+        <b-form-input v-model="password"></b-form-input>
+        <b-input-group-append>
+          <b-button variant="info">Unlock</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-row>
+    <b-row v-if="auth">
+      <test-component></test-component>
     </b-row>
   </b-container>
   `,
   data() {
     return {
       password: '',
-
+      auth: false
     }
   },
   methods: {
     unlock() {
-      var code = CryptoJS.AES.decrypt(encrypted_js.value, "ankoor");
+      var self = this;
+      var code = CryptoJS.AES.decrypt(encrypted_js.value, self.password);
       var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
       var script = "<script type='text/javascript'> " + decryptedMessage + " </script>";
       $('body').append(script);
+      setTimeout(function() {
+        self.auth = true;
+      },2000)
     }
   }
 })
