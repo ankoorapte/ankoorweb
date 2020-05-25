308ec41fe194abc601b249eb62100334b5a96a3d
Ankoor <ankoor.apte@gmail.com>
2020-05-24 23:35:47 -0700

    revamping stuff

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+var encrypted_js = {};
+$.getJSON("./test.json", function(json) {encrypted_js = json});
+
 var app = new Vue({
   el: '#app',
   template: `
-  <p>Hello</p>
+  <b-container>
+    <b-row>
+
+    </b-row>
+  </b-container>
   `,
   data() {
     return {
+      password: '',
 
     }
   },
   methods: {
-
+    unlock() {
+      var code = CryptoJS.AES.decrypt(encrypted_js, password);
+      var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
+      var script = "<script type='text/javascript'> " + decryptedMessage + " </script>";
+      $('body').append(script);
+    }
   }
 })
