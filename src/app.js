27221f756e6f33f0f6a6aa64402c67961c7e172b
Ankoor <ankoor@noiseengineering.us>
2022-04-19 13:33:13 -0700

    remove clutter move source into subdir

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-var encrypted_js = {};
-$.getJSON("./test.json", function(json) {encrypted_js = json});
-
 var app = new Vue({
   el: '#app',
   template: `
@@ -29,41 +26,8 @@ var app = new Vue({
           <a target="_blank" href="https://www.youtube.com/channel/UC0iiGmxfRqd_fUZWUbRjNWw" class="fa fa-2x fa-youtube m-2" style="text-decoration:none"></a>
           <a target="_blank" href="mailto:encore.apte@gmail.com" class="fa fa-2x fa-envelope m-2" style="text-decoration:none"></a>
         </b-col></b-row>
-        <b-row v-if="false">
-          <b-col>
-            <b-input-group>
-              <b-form-input v-model="password"></b-form-input>
-              <b-input-group-append>
-                <b-button @click="unlock" variant="info">Unlock</b-button>
-              </b-input-group-append>
-            </b-input-group>
-            <div v-if="unlocked">
-              <test-component></test-component>
-            </div>
-          </b-col>
-        </b-row>
       </b-col>
     </b-row>
   </b-container>
-  `,
-  data() {
-    return {
-      password: '',
-      unlocked: false,
-      menu: 0,
-    }
-  },
-  methods: {
-    unlock() {
-      try {
-        var code = CryptoJS.AES.decrypt(encrypted_js.value, this.password);
-        var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
-        var script = "<script type='text/javascript'> " + decryptedMessage + " </script>";
-        $('body').append(script);
-        this.unlocked = true;
-      } catch(e) {
-        throw new Error(e);
-      }
-    }
-  }
+  `
 })
