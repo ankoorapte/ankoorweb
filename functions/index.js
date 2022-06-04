fb55951c645b6c8f999e9e30439ea907b1049986
Ankoor <ankoor.apte@gmail.com>
2022-06-04 13:07:12 -0700

    bucket trigger

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,19 @@
 const functions = require("firebase-functions");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// gcloud functions deploy helloGCS --runtime nodejs16
+// --trigger-resource player-76353.appspot.com
+// --trigger-event google.storage.object.finalize
+exports.helloGCS = (file, context) => {
+  console.log(`  Event: ${context.eventId}`);
+  console.log(`  Event Type: ${context.eventType}`);
+  console.log(`  Bucket: ${file.bucket}`);
+  console.log(`  File: ${file.name}`);
+  console.log(`  Metageneration: ${file.metageneration}`);
+  console.log(`  Created: ${file.timeCreated}`);
+  console.log(`  Updated: ${file.updated}`);
+};
