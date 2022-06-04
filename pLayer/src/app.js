89e96557547e20f45a7f2dca40632444b0fd7683
Ankoor <ankoor.apte@gmail.com>
2022-06-04 11:36:17 -0700

    try new config structure

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -1,27 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-analytics.js";
-import { getStorage, ref } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-storage.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDzJylYhhlw9LVay0OUkAyMmR9vYJsXr8U",
-  authDomain: "player-76353.firebaseapp.com",
-  projectId: "player-76353",
-  storageBucket: "player-76353.appspot.com",
-  messagingSenderId: "954598460815",
-  appId: "1:954598460815:web:52ae341cbaf40a1c6e8ffa",
-  measurementId: "G-VK3H3Y1430"
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
-const storage = getStorage(firebaseApp);
 const storageRef = ref(storage, 'public');
 var app = new Vue({
   el: '#app',
