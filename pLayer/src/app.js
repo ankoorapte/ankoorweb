5cec4e22b2ec7e248e05d41aafa941e392a3dc70
Ankoor <ankoor.apte@gmail.com>
2022-06-04 10:33:38 -0700

    fix file drop

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -4,7 +4,7 @@ var app = new Vue({
   <b-container style="background-color:#E0FFF2">
     <b-row class="m-1 p-1">
       <b-col align="center">
-        <b-form-file v-model="file2" class="mt-3" plain></b-form-file>
+        <b-form-file v-model="file" class="mt-3" plain></b-form-file>
         <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
       </b-col>
     </b-row>
