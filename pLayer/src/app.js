23d5c3c14abc36a28b61ea1e2a04944cd561311b
Ankoor <ankoor.apte@gmail.com>
2022-06-05 19:12:59 -0700

    remove auth

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -18,34 +18,6 @@ const storage = getStorage(firebaseApp);
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
 
-// FirebaseUI config.
-var uiConfig = {
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    auth.GoogleAuthProvider.PROVIDER_ID,
-    auth.FacebookAuthProvider.PROVIDER_ID,
-    auth.TwitterAuthProvider.PROVIDER_ID,
-    auth.GithubAuthProvider.PROVIDER_ID,
-    auth.EmailAuthProvider.PROVIDER_ID,
-    auth.PhoneAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-  ],
-  // tosUrl and privacyPolicyUrl accept either url string or a callback
-  // function.
-  // Terms of service url/callback.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url/callback.
-  privacyPolicyUrl: function() {
-    window.location.assign('<your-privacy-policy-url>');
-  }
-};
-
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
-
 let L1 = {};
 let L1_keys = [];
 
