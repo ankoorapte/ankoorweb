c1a2f8c67939863537e994f5c2ead0101722b8a9
Ankoor <ankoor.apte@gmail.com>
2022-06-07 13:55:46 -0700

    audio stuff

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -46,6 +46,7 @@ let users = {};
 let audioTest = async (audio1, audio2) => {
   console.log(audio1);
   const ac = new AudioContext();
+  ac.resume();
   const merger = ac.createChannelMerger(2);
   const dest = ac.createMediaStreamDestination();
   let data1 = await ac.decodeAudioData(audio1);
