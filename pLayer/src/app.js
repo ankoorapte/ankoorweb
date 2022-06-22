c7ffb2946c7518b9c391e27ebaf63083186d52ea
Ankoor <ankoor.apte@gmail.com>
2022-06-22 12:29:07 -0700

    GUI changes

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -120,14 +120,7 @@ let app = new Vue({
             <p align="center" v-if="user"><b>hello, {{ user.displayName }}</b></p>
             <p align="center"><b-button variant="danger" @click="signOut">sign out</b-button></p>
             <hr>
-            <b-form-group
-              :invalid-feedback="invalidUsername"
-              :state="stateUsername"
-              align="center" 
-              class="w-75"
-            >
-              <b-form-input placeholder="new username" @keydown.native="usernameKeydownHandler" v-model="newUsername" :state="stateUsername" trim></b-form-input>
-            </b-form-group>
+            <b-form-input align="center" :invalid-feedback="invalidUsername" :state="stateUsername" class="w-75" placeholder="new username" @keydown.native="usernameKeydownHandler" v-model="newUsername" :state="stateUsername" trim></b-form-input>
             <p align="center"><b-button variant="primary" :disabled="posting || !newUsername" @click="changeUsername(0)">update username</b-button></p>
           </b-tab>
         </b-tabs>
