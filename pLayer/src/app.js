aca748f08891d0dcb867eb99733c27e6f7a79031
Ankoor <ankoor.apte@gmail.com>
2022-10-27 12:07:30 -0700

    try fix

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -64,7 +64,7 @@ let app = new Vue({
       </b-col>
       <b-col align="center">
         <b-spinner v-show="busy" variant="dark" type="grow"></b-spinner> 
-        <>pLayer</b>
+        <b>pLayer</b>
       </b-col>
       <b-col align="right">
         <b-button variant="outline-dark" @click="showSettings = !showSettings"><b-icon icon="wrench"></b-icon></b-button>
