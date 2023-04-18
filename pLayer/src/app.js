d3671eb74f092fb2dd187d431ca9aad42cae5ca7
Ankoor <ankoor.apte@gmail.com>
2023-04-17 17:10:30 -0700

    GUI

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -118,8 +118,8 @@ let app = new Vue({
           </b-input-group-append>
         </b-input-group>
       </b-col></b-row>
-      <b-row><b-col align="center">
-        <a class="mb-2" href="https://forms.gle/TSSQvBinSwGLrnyT6" target="_blank">Report feedback</a>
+      <b-row class="mb-2"><b-col align="center">
+        <a href="https://forms.gle/TSSQvBinSwGLrnyT6" target="_blank">Report feedback</a>
       </b-col></b-row>
       <b-row><b-col align="center">
         <b-button v-show="!busy" variant="danger" @click="signOut">sign out</b-button>
