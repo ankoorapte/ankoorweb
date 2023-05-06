00392c15dde0339cd22a57aa8b934b5f1032acfc
Ankoor <ankoor.apte@gmail.com>
2023-05-06 11:10:49 -0700

    bla

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -446,7 +446,6 @@ let app = new Vue({
       }
     }, 
     async getTrack(draftLayer="") {
-      console.log("hi");
       if(!Object.keys(tracks).length) return;
       this.busy = true;
       let trackLayers = tracks[this.trackID].layers.slice();
@@ -591,6 +590,7 @@ let app = new Vue({
       return layers[uid].name;
     },
     getLayerURL(buffer) {
+      console.log("hi");
       return window.URL.createObjectURL(new Blob([buffer], { type: "audio/wav" }));
     },
     getBaseUser(uid) {
