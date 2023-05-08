cd45f88cf01c75d64e741ec6d5879e2857e89330
Ankoor <ankoor.apte@gmail.com>
2023-05-08 13:22:32 -0700

    try fixing mobile

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -145,7 +145,11 @@ let app = new Vue({
           <p v-show="draft.length" style="font-size:14px">
             <i>draft version with new layer <b>{{getLayerName(draft)}}</b></i>
           </p>
-          <b-list-group v-for="(layer_item, index) in layerBuffers" v-bind:key="index">
+          <p v-if="isMobile">
+            <b-button :disabled="busy" variant="dark" @click="togglePlay()" v-show="!paused"><b-icon icon="pause-fill"></b-icon></b-button>
+            <b-button :disabled="busy" variant="dark" @click="togglePlay()" v-show="paused"><b-icon icon="play-fill"></b-icon></b-button>
+          </p>
+          <b-list-group v-if="!isMobile" v-for="(layer_item, index) in layerBuffers" v-bind:key="index">
             <b-list-group-item class="p-0 d-flex justify-content-between align-items-center">
               <b-col>
                 <p style="font-size:14px" class="mb-0"> 
@@ -274,6 +278,7 @@ let app = new Vue({
       trackIdx: 0,
       layering: false,
       paused: true,
+      audioContext: null,
       merger: null,
       mixedAudio: null,
       seeker: 0,
@@ -293,6 +298,7 @@ let app = new Vue({
       self.busy = true;
       if(user) { await self.signIn(user); }
       if(self.signedIn) {
+        self.resetAudioContext();
         unsubscribe_layers = onSnapshot(collection(db, "layers"), (layerDocs) => {
           layers = {};
           layerDocs.forEach((doc) => {
@@ -339,6 +345,11 @@ let app = new Vue({
     },
     stateEmail() {
       return this.newEmail.includes("@") && this.email.includes(".");
+    },
+    isMobile() {
+      let check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
     }
   },
   methods: {
@@ -403,6 +414,8 @@ let app = new Vue({
       }
     },
     async signOut() {
+      this.resetAudioContext();
+      await this.forcePause();
       unsubscribe_tracks();
       unsubscribe_layers();
       unsubscribe_users();
@@ -436,12 +449,20 @@ let app = new Vue({
       });
       await this.signOut();
     },
+    resetAudioContext() {
+      this.audioContext = new AudioContext();
+      this.merger = this.audioContext.createChannelMerger(2);
+      this.mixedAudio = this.audioContext.createMediaStreamDestination();
+      this.merger.connect(this.mixedAudio);
+      this.merger.connect(this.audioContext.destination);
+    },
     async getLayerBuffer(layerID) {
       let data = await (await fetch(await getDownloadURL(ref(storage, layerID)))).arrayBuffer()
       return {
         id: layerID,
         user: layers[layerID].user,
-        data: data.slice()
+        data: data.slice(),
+        decoded_data: await this.audioContext.decodeAudioData(data)
       }
     }, 
     async getTrack(draftLayer="") {
@@ -503,7 +524,29 @@ let app = new Vue({
         self.inactiveLayers = self.inactiveLayers.filter((l) => l != layerID);
       }
     },
+    async forcePause() {
+      if(!this.paused) await this.togglePlay();
+    },
+    async togglePlay() {
+      if(!this.isMobile()) return;
+      if(this.paused) {
+        this.resetAudioContext();
+        for(const layerBuffer of this.layerBuffers) {
+          let source = this.audioContext.createBufferSource();
+          source.buffer = layerBuffer.decoded_data;
+          source.connect(this.merger, 0, 0);
+          source.connect(this.merger, 0, 1);
+          source.start(0, this.seeker);
+          this.layers.push(source);
+        }
+      } else {
+        this.seeker += this.audioContext.currentTime;
+        this.layers.forEach((node) => node.stop());
+      }
+      this.paused = !this.paused;
+    },
     async toggleTrack(forward) {
+      await this.forcePause();
       this.busy = true;
       if(forward) { this.trackIdx++; }
       else { 
@@ -518,6 +561,7 @@ let app = new Vue({
     },
     async post() {
       let self = this;
+      await self.forcePause();
       self.busy = true;
       const uid = uuidv4();
       const layerPath = ref(storage, uid);
@@ -563,19 +607,23 @@ let app = new Vue({
       this.group_discography = Object.keys(tracks).map((t) => {return {trackID:t}});
     },
     async playDiscography(index) {
+      await this.forcePause();
       this.trackID = this.discography[index].trackID;
       await this.getTrack();
     },
     async playGroupDiscography(index) {
+      await this.forcePause();
       this.trackID = this.group_discography[index].trackID;
       await this.getTrack();
     },
     async playDraft(index, whichbox) {
+      await this.forcePause();
       this.seeker = 0;
       this.trackID = this[whichbox][index].baseID
       await this.getTrack(this[whichbox][index].layerID);
     },
     async resolveDraft(index, accept) {
+      await this.forcePause();
       let layerID = this.inbox[index].layerID;
       let baseID = this.inbox[index].baseID;
       await this.pLayerAPI("resolveLayer",{
