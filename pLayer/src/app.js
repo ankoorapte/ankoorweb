3c7af54ad906e06fcc1d000955ac39bd34893994
Ankoor <ankoor.apte@gmail.com>
2023-05-12 21:02:51 -0700

    GUI

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -211,7 +211,7 @@ let app = new Vue({
             <b-tabs card align="center">
               <b-tab>
                 <template #title>
-                  <p style="font-size:10px">requests {{inbox.length ? "(" + inbox.length + ")" : ""}}</p>
+                  <p style="font-size:14px" class="m-0">requests {{inbox.length ? "(" + inbox.length + ")" : ""}}</p>
                 </template>
                 <b-list-group v-for="(inbox_item, index) in inbox" v-bind:key="inbox_item.layerID">
                   <b-list-group-item class="p-0 d-flex justify-content-between align-items-center">
@@ -228,7 +228,7 @@ let app = new Vue({
               </b-tab>
               <b-tab>
                 <template #title>
-                  <p style="font-size:10px">submissions {{outbox.length ? "(" + outbox.length + ")" : ""}}</p>
+                  <p style="font-size:14px" class="m-0">submissions {{outbox.length ? "(" + outbox.length + ")" : ""}}</p>
                 </template>
                 <b-list-group v-for="(outbox_item, index) in outbox" v-bind:key="outbox_item.layerID">
                   <b-list-group-item class="p-0 d-flex justify-content-between align-items-center">
@@ -241,7 +241,7 @@ let app = new Vue({
               </b-tab>
               <b-tab active>
                 <template #title>
-                  <p style="font-size:10px">done</p>
+                  <p style="font-size:14px" class="m-0">done</p>
                 </template>
                 <b-list-group v-for="(disco_item, index) in discography" v-bind:key="disco_item.trackID">
                   <b-list-group-item class="p-0 d-flex justify-content-between align-items-center">
