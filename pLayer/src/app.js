d4e525112b0b87edd6e7a46d145699aabff75564
Ankoor <ankoor.apte@gmail.com>
2023-05-15 19:30:22 -0700

    GUI

--- pLayer/src/app.js
+++ pLayer/src/app.js
@@ -229,7 +229,7 @@ let app = new Vue({
                 <b-badge href="#" variant="info" @click="layering = true; tabIndex = 0;">layer</b-badge>
               </p>
           </b-list-group-item>
-          <b-list-group-item class="p-1">
+          <b-list-group-item class="p-0 d-flex justify-content-between align-items-center">
             <b-input-group>
               <template #prepend>
                 <b-button class="p-1" variant="dark" @click="toggleTrack(0)"><b-icon icon="skip-backward-fill"></b-icon></b-button>
@@ -237,7 +237,7 @@ let app = new Vue({
                 <b-button class="p-1" variant="dark" @click="togglePlay()" v-show="paused"><b-icon icon="play-fill"></b-icon></b-button>
                 <b-button class="p-1" variant="dark" @click="toggleTrack(1)"><b-icon icon="skip-forward-fill"></b-icon></b-button>
               </template>
-              <b-form-input v-if="!busy" class="w-75" type="range" @input="seekerInput" v-model="slider" min="0" :max="trackDuration" step="0.1"></b-form-input>
+              <b-form-input v-if="!busy" class="w-75 ml-2" type="range" @input="seekerInput" v-model="slider" min="0" :max="trackDuration" step="0.1"></b-form-input>
               <template #append>
                 {{ trackTimestamp(slider) }}/{{ trackTimestamp(trackDuration) }}
               </template>
