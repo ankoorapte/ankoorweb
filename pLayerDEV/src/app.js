bea5b3b16e84f06f927b805ac2d20f4eaa995cd7
Ankoor <ankoor.apte@gmail.com>
2023-06-04 16:12:30 -0700

    GUI

--- pLayerDEV/src/app.js
+++ pLayerDEV/src/app.js
@@ -123,7 +123,7 @@ let app = new Vue({
       return this.password.length >= 6 && this.email.includes("@") && this.email.includes(".");
     },
     stateGroup() {
-      return this.newGroupName.length && this.newGroupUsers.split(" ").every((email) => Object.keys(this.users).map((uid) => this.users[uid].email).includes(email));
+      return Boolean(this.newGroupName.length) && this.newGroupUsers.split(" ").every((email) => Object.keys(this.users).map((uid) => this.users[uid].email).includes(email));
     },
     stateUsername() {
       return this.user
