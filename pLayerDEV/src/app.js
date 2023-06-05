c73ec1ce3ab3e7952562734e9fe231734894b77d
Ankoor <ankoor.apte@gmail.com>
2023-06-05 13:51:38 -0700

    fixed

--- pLayerDEV/src/app.js
+++ pLayerDEV/src/app.js
@@ -202,7 +202,7 @@ let app = new Vue({
     <b-navbar v-if="signedIn" variant="faded" fixed="bottom" type="dark">
       <b-col align="center">
         <b-spinner v-show="busy" variant="dark" type="grow"></b-spinner>
-        <b-button-group v-if="activeTrack.length > 0" size="lg" class="mb-2" v-if="!busy">
+        <b-button-group v-if="!busy && activeTrack.length > 0" size="lg" class="mb-2">
           <b-button class="p-1" variant="dark" @click="toggleTrack(0)"><b-icon icon="skip-backward-fill"></b-icon></b-button>
           <b-button class="p-1" variant="dark" @click="pause()" v-show="!paused"><b-icon icon="pause-fill"></b-icon></b-button>
           <b-button class="p-1" variant="dark" @click="play()" v-show="paused"><b-icon icon="play-fill"></b-icon></b-button>
