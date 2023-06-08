9546bda181b70360360dffa8f4c2a228933729ab
Ankoor <ankoor.apte@gmail.com>
2023-06-08 15:36:44 -0700

    GUI

--- functions/player.js
+++ functions/player.js
@@ -204,6 +204,7 @@ class Player {
     }
 
     timeline.sort((a, b) => a.when.toMillis() - b.when.toMillis());
+    timeline.forEach((event) => event.when = event.when.toDate());
     return {status: "ok", data: timeline};
   }
   async addComment(arg) {
