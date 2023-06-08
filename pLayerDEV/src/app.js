6bd1ef90405991d22650b4203395be3e06bbabd4
Ankoor <ankoor.apte@gmail.com>
2023-06-08 16:10:25 -0700

    GUI

--- pLayerDEV/src/app.js
+++ pLayerDEV/src/app.js
@@ -39,7 +39,7 @@ let app = new Vue({
   el: '#app',
   // GUI
   template: `
-  <b-container style="background-color:#99CAFF;">
+  <b-container>
     <b-row style="font-size:30px" class="mb-1">
       <b-col align="center" class="d-flex justify-content-between align-items-center">
         <b-button v-if="signedIn" v-b-toggle.sidebar-group variant="outline-dark"><b-icon icon="people"></b-icon></b-button>
