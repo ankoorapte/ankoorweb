ddb11ce8bf2435da1e415cfa05400fd6789eac79
Ankoor <ankoor.apte@gmail.com>
2023-06-08 18:40:44 -0700

    GUI

--- pLayerDEV/src/app.js
+++ pLayerDEV/src/app.js
@@ -219,7 +219,7 @@ let app = new Vue({
         <b-collapse v-model="showTimeline">
           <b-list-group v-if="!busy && activeTrack.length > 0" flush>
             <b-list-group-item :disabled="busy" class="p-0">
-              <b-card style="max-height:300px; overflow: auto; flex-direction: column-reverse" no-header class="w-100 m-0 p-0">
+              <b-card style="max-height:300px; overflow: scroll; flex-direction: column-reverse" no-header class="w-100 m-0 p-0">
                 <b-list-group v-for="(timeline_item, index) in timeline" v-bind:key="timeline_item.when" flush>
                   <b-row class="m-0 p-0"><b-col class="m-0 p-0 d-flex justify-content-between align-items-center">
                     <p style="font-size:12px" class="m-0 p-0 mr-auto"><b>{{getUserName(timeline_item.user)}}: </b> {{timeline_item.message}}</p>
