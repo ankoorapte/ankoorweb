7c1ace3596c32b88734d0830d1dfb36cd78574df
Ankoor <ankoor.apte@gmail.com>
2023-06-08 18:48:24 -0700

    GUI

--- pLayerDEV/src/app.js
+++ pLayerDEV/src/app.js
@@ -142,6 +142,9 @@ let app = new Vue({
       </b-card>
     </b-col></b-row>
     <b-collapse v-model="hideLayers"><b-row v-if="signedIn">
+      <b-col v-if="!activeGroup.length">
+        <p class="m-auto">welcome, {{user.displayName}}</p>
+      </b-col>
       <b-col v-if="activeGroup.length > 0">
         <b-form-group :description="getGroupUsers(activeGroup)" align="center">
           <b-input-group class="my-1">
