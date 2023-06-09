7e38a0b9c32147fa141a9725b2941622e2ef60b7
Ankoor <ankoor.apte@gmail.com>
2023-06-08 18:30:33 -0700

    GUI

--- pLayerDEV/src/app.js
+++ pLayerDEV/src/app.js
@@ -222,8 +222,8 @@ let app = new Vue({
               <b-card style="max-height:300px; overflow: auto;" no-header class="w-100 m-0 p-0">
                 <b-list-group v-for="(timeline_item, index) in timeline" v-bind:key="timeline_item.when" flush>
                   <b-row class="m-0 p-0"><b-col class="m-0 p-0 d-flex justify-content-between align-items-center">
-                    <p style="font-size:12px" class="m-0 p-0"><b>{{getUserName(timeline_item.user)}}: </b> {{timeline_item.message}}</p>
-                    <p style="font-size:12px" class="m-0 p-0 text-secondary">{{getTimelineTimestamp(timeline_item.when)}}</p>
+                    <p style="font-size:12px" class="m-0 p-0 mr-auto"><b>{{getUserName(timeline_item.user)}}: </b> {{timeline_item.message}}</p>
+                    <p style="font-size:12px" class="m-0 p-0 ml-auto text-secondary">{{getTimelineTimestamp(timeline_item.when)}}</p>
                   </b-col></b-row>
                 </b-list-group>
                 <b-input-group class="m-0 p-0" style="font-size:12px" :disabled="busy">
