24e0d97fe4d1d272f3d0614f7c78f36b27529654
Ankoor Apte <a_apte@apple.com>
2024-12-15 16:21:39 +0530

    gui changes to get away from pLayer

--- chat/src/app.js
+++ chat/src/app.js
@@ -158,33 +158,6 @@ let app = new Vue({
     }
   },
   watch: {
-    // whenever question changes, this function will run
-    activeGroup(newGroup, oldGroup) {
-      let self = this;
-      self.pause();
-      self.groupTracks = Object.keys(self.tracks).filter((trackID) => self.layers[trackID].group === newGroup).map((trackID) => {
-        return {
-          uid: trackID,
-          name: self.tracks[trackID].name,
-          layers: self.tracks[trackID].layers
-        }
-      });
-      this.showNewGroup = !newGroup.length;
-      this.activeTrack = "";
-      this.showLayers = false;
-      this.showNewTrack = false;
-    },
-    async activeTrack(newTrack, oldTrack) {
-      this.busy = true;
-      await this.pause()
-      await this.getTrack();
-      if(newTrack.length) await this.play();
-      else this.showNewTrack = false;
-      this.busy = false;
-    },
-    showLayers(newBool, oldBool) {
-      if(!newBool) this.showNewLayer = false;
-    }
   },
   async created() {
     let self = this;
@@ -205,9 +178,6 @@ let app = new Vue({
     },
     stateCredentials() {
       return this.password.length >= 6 && this.email.includes("@") && this.email.includes(".");
-    },
-    stateGroup() {
-      return Boolean(this.newGroupName.length) && this.newGroupUsers.split(" ").filter((s) => s.length).every((email) => Object.keys(this.users).map((uid) => this.users[uid].email).includes(email));
     },
     stateAddUser() {
       return Object.keys(this.users).map((uid) => this.users[uid].email).includes(this.userToAdd);
@@ -247,44 +217,6 @@ let app = new Vue({
       let check = false;
       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
       return check;
-    },
-    async pLayerAPI(endpoint = "", arg = {}) {
-      this.busy = true;
-      if(endpoint != "createUser") arg['id'] = await this.user.getIdToken(/* forceRefresh */ true);
-      arg['endpoint_name'] = endpoint;
-      let res = await fetch('https://us-central1-player-76353.cloudfunctions.net/pLayerAPI',{
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify(arg)
-      });
-      let res_json = await res.json();
-      this.busy = false;
-      if(res_json['stack'] && res_json['stack'].slice(0,5) == "Error") {
-        throw new Error(res_json.message);
-      } else if(res_json['status'] && res_json['status'] == "ok") {
-        return res_json['data'];
-      } else {
-        throw new Error("No status found in response");
-      }
-    },
-    async updateDB() {
-      let self = this;
-      const db = await self.pLayerAPI("getDB");
-      self.tracks = db.tracks;
-      self.layers = db.layers;
-      self.users = db.users;
-      self.groups = db.groups;
-      self.myGroups = Object.keys(self.groups).filter((groupID) => self.groups[groupID].users.includes(self.user.uid)).map((uid) => {
-        return {
-          uid: uid,
-          name: self.groups[uid].name,
-          users: self.groups[uid].users.map(this.getUserName)
-        }
-      });
-      if(self.activeTrack.length) self.timeline = await self.pLayerAPI("getTimeline", { trackID: self.activeTrack });
     },
     async createUser() {
       let self = this;
@@ -334,18 +266,6 @@ let app = new Vue({
       this.users = {};
       await signOut(auth);
     },
-    async createGroup() {
-      let self = this;
-      const newGroupUserList = self.newGroupUsers.split(" ").filter((s) => s.length);
-      await self.pLayerAPI("createGroup", {
-        name: self.newGroupName,
-        groupID: uuidv4(),
-        users: Object.keys(self.users).filter((uid) => newGroupUserList.includes(self.users[uid].email))
-      });
-      await self.updateDB();
-      self.showNewGroup = false;
-      self.newGroupUsers = "";
-    },
     async changeUsername(un) {
       if(!un) un = this.newUsername;
       this.user.displayName = this.newUsername;
@@ -370,310 +290,6 @@ let app = new Vue({
       });
       await this.signOut();
     },
-    async changeGroupName() {
-      let name = this.activeGroupName;
-      let gid = this.activeGroup;
-      await this.pLayerAPI("updateGroup",{
-        groupID: gid,
-        field: "name",
-        value: name
-      });
-      await this.updateDB();
-    },
-    async addUser() {
-      let newUser = Object.keys(this.users).filter((uid) => this.users[uid].email == this.userToAdd)[0];
-      let gid = this.activeGroup;
-      await this.pLayerAPI("updateGroup",{
-        groupID: gid,
-        field: "users",
-        value: newUser
-      });
-      this.showAddUser = false;
-      await this.updateDB();
-    },
-    async seekerInput(seek) {
-      clearInterval(this.interval);
-      this.seeker = parseFloat(seek);
-      await this.pause();
-      await this.play();
-    },
-    updateSlider() {
-      this.slider = this.seeker + this.audioContext.currentTime;
-      if(this.slider > this.trackDuration) {
-        clearInterval(this.interval);
-      }
-    },
-    resetAudioContext() {
-      this.audioContext = new AudioContext();
-      this.merger = this.audioContext.createChannelMerger(2)
-      this.merger.connect(this.audioContext.createMediaStreamDestination());
-      this.merger.connect(this.audioContext.destination);
-    },
-    async getLayerBuffer(layerID) {
-      let fetch_res = await fetch(await getDownloadURL(ref(storage, layerID)));
-      let data = await fetch_res.arrayBuffer();
-      return {
-        id: layerID,
-        name: this.layers[layerID].name,
-        user: this.layers[layerID].user,
-        data: data.slice(),
-        decoded_data: await this.audioContext.decodeAudioData(data)
-      }
-    }, 
-    async getTrack(draftLayer="", subLayer="") {
-      let self = this;
-      await self.pause();
-      if(!self.groupTracks.length || !self.activeTrack.length) return;
-      self.draft = draftLayer;
-      self.busy = true;
-      let trackLayers = self.tracks[self.activeTrack].layers.map((layerID) => self.layers[layerID].revisions.slice(-1)[0]);
-      if(draftLayer.length) {
-        if(subLayer.length) {
-          trackLayers[trackLayers.findIndex((layerID) => subLayer === layerID)] = draftLayer;
-        } else {
-          trackLayers.push(draftLayer);
-        }
-      }
-      self.layerBuffers = await Promise.all(trackLayers.map(self.getLayerBuffer));
-      self.layerMute = Array(trackLayers.length).fill(false);
-      self.seeker = 0;
-      self.slider = 0;
-      self.trackDuration = self.layerBuffers[0].decoded_data.duration;
-      self.trackIdx = self.groupTracks.findIndex((track) => track.uid == self.activeTrack);
-      self.timeline = await self.pLayerAPI("getTimeline", { trackID: self.activeTrack });
-      self.busy = false;
-    },
-    async pause() {
-      if(!this.paused) await this.togglePlay();
-    },
-    async play() {
-      if(this.paused) await this.togglePlay();
-    },
-    async togglePlay() {
-        if(this.paused) {
-          this.resetAudioContext();
-          for(const idx in this.layerBuffers) {
-            var gainNode = this.audioContext.createGain();
-            gainNode.gain.value = this.layerMute[idx] ? 0 : 1;
-            gainNode.connect(this.merger, 0, 0);
-            gainNode.connect(this.merger, 0, 1);
-            this.layerGains.push(gainNode);
-            let source = this.audioContext.createBufferSource();
-            source.buffer = this.layerBuffers[idx].decoded_data;
-            source.connect(gainNode);
-            source.start(0, this.seeker);
-            this.trackLayers.push(source);
-            let self = this;
-            source.onended = () => {
-              if(Math.ceil(self.slider) == Math.ceil(self.trackDuration)) {
-                self.pause();
-                self.slider = 0;
-                self.seeker = 0;
-              }
-            }
-          }
-          this.interval = setInterval(this.updateSlider, 100);
-        } else {
-          clearInterval(this.interval);
-          this.seeker += this.audioContext.currentTime;
-          this.trackLayers.forEach((node) => node.stop());
-          this.trackLayers = [];
-          this.layerGains = [];
-        }
-      this.paused = !this.paused;
-    },
-    async toggleTrack(forward) {
-      await this.pause();
-      this.busy = true;
-      if(forward) { this.trackIdx++; }
-      else { 
-        if(!this.trackIdx) this.trackIdx = this.groupTracks.length;
-        this.trackIdx--;
-      }
-      this.trackIdx = this.trackIdx % this.groupTracks.length;
-      this.activeTrack = this.groupTracks[this.trackIdx].uid;
-      await this.getTrack();
-      this.busy = false;
-    },
-    async postTrack() {
-      let self = this;
-      self.busy = true;
-      const uid = uuidv4();
-      const trackPath = ref(storage, uid);
-      const metadata = {
-        customMetadata: {
-          'name': self.newTrackName,
-          'user': self.user.uid,
-          'base': "",
-          'sub': "",
-          'bpm': self.newTrackBPM,
-          'group': self.activeGroup
-        },
-        contentType: 'audio/wav'
-      }; 
-      await uploadBytes(trackPath, self.newTrack, metadata);
-      self.newTrackName = "";
-      self.newTrackBPM = "";
-      self.newTrack = null;
-      self.showNewTrack = false;
-      self.busy = false;
-      await self.updateDB();
-    },
-    async postLayer() {
-      let self = this;
-      self.busy = true;
-      const uid = uuidv4();
-      const trackPath = ref(storage, uid);
-      const metadata = {
-        customMetadata: {
-          'name': self.newLayerName,
-          'user': self.user.uid,
-          'base': self.activeTrack,
-          'sub': "",
-          'bpm': self.getTrackBPM(self.activeTrack),
-          'group': self.activeGroup
-        },
-        contentType: 'audio/wav'
-      }; 
-      await uploadBytes(trackPath, self.newLayer, metadata);
-      self.newLayerName = "";
-      self.newLayer = null;
-      self.busy = false;
-      self.showLayers = false;
-      self.showNewLayer = false;
-      await self.updateDB();
-    },
-    async postSubstitution() {
-      let self = this;
-      self.busy = true;
-      const uid = uuidv4();
-      const trackPath = ref(storage, uid);
-      const metadata = {
-        customMetadata: {
-          'name': self.layers[self.subLayer].name,
-          'user': self.user.uid,
-          'base': self.activeTrack,
-          'sub': self.subLayer,
-          'bpm': self.getTrackBPM(self.activeTrack),
-          'group': self.activeGroup
-        },
-        contentType: 'audio/wav'
-      }; 
-      await uploadBytes(trackPath, self.newSub, metadata);
-      self.newLayerName = "";
-      self.subLayer = "";
-      self.newLayer = null;
-      self.busy = false;
-      self.showLayers = false;
-      self.showNewLayer = false;
-      await self.updateDB();
-    },
-    soloLayer(index) {
-      for(const idx in this.layerBuffers) {
-        if(idx === index.toString()) {
-          this.unmuteLayer(idx);
-        } else {
-          this.muteLayer(idx);
-        }
-      }
-    },
-    muteLayer(index) {
-      this.layerMute[index] = true;
-      this.layerGains[index].gain.value = 0;
-    },
-    unmuteLayer(index) {
-      this.layerMute[index] = false;
-      this.layerGains[index].gain.value = 1;
-    },
-    async uploadSubstitute(layerID) {
-      this.subLayer = layerID;
-      this.$refs['newSub'].childNodes[1].childNodes[0].click();
-    },
-    async downloadLayer(index) {
-      this.busy = true;
-      let blob = await (await fetch(await getDownloadURL(ref(storage, this.layerBuffers[index].id)))).blob();
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      a.download = this.layerBuffers[index].name + ".wav";
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-      this.busy = false;
-    },
-    async addComment() {
-      let self = this;
-      let arg = {
-        trackID: self.activeTrack,
-        message: self.newComment
-      }
-      await self.pLayerAPI("addComment", arg);
-      self.timeline = await self.pLayerAPI("getTimeline", arg);
-      self.newComment = "";
-    },
-    async resolveDraft(layerID, subLayerID, accept) {
-      const baseID = this.activeTrack;
-      const endpoint = subLayerID.length ? "resolveSubstitution" : "resolveLayer";
-      await this.pause();
-      await this.pLayerAPI(endpoint,{
-        layerID: layerID,
-        baseID: baseID,
-        subLayerID: subLayerID,
-        accept: accept
-      });
-      this.draft = "";
-      await this.updateDB();
-    },
-    showResolve(layerID) {
-      return this.draft.length > 0 &&
-            this.draft === layerID && 
-            this.user.uid === this.tracks[this.activeTrack].user;
-    },
-    async detectBPM() {
-      if(this.newTrack) {
-        let ac = new AudioContext();
-        this.newTrackBPM = bpmDetective(await ac.decodeAudioData(await this.newTrack.arrayBuffer())).toString();  
-      }
-    },
-    trackTimestamp(seconds) {
-      let minutes = Math.floor(seconds / 60);
-      let extraSeconds = seconds % 60;
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds;
-      return minutes + ":" + extraSeconds.toString().slice(0, 2);
-    },
-    getTimelineTimestamp(when) {
-      const d = new Date(when);
-      return d.toLocaleString();
-    },
-    getGroupUsers(uid) {
-      if(!uid || !Object.keys(this.groups).length) return [];
-      return this.groups[uid].users.map(this.getUserName).join(", ");
-    },
-    getTrackBPM(uid) {
-      if(!uid || !Object.keys(this.tracks).length) return [];
-      return this.layers[uid].bpm;
-    },
-    getTrackName(uid) {
-      if(!uid || !Object.keys(this.tracks).length) return "";
-      return this.tracks[uid].name;
-    },
-    getTrackArtists(uid) {
-      if(!uid || !Object.keys(this.tracks).length) return [];
-      return [...new Set(this.tracks[uid].layers.map((layerID) => this.getUserName(this.layers[layerID].user)))];
-    },
-    getLayerName(uid) {
-      if(!uid || !Object.keys(this.layers).length) return "";
-      return this.layers[uid].name;
-    },
-    getLayerUser(uid) {
-      if(!uid || !Object.keys(this.layers).length) return [];
-      return this.layers[uid].user;
-    },
-    getLayerId(name) {
-
-    },
     getUserName(uid) {
       if(!uid || !Object.keys(this.users).length) return [];
       return this.users[uid].displayName;
@@ -682,12 +298,7 @@ let app = new Vue({
       if (event.which === 13 && this.stateUsername) {
         this.changeUsername();
       }
-    },    
-    commentKeydownHandler(event) {
-      if (event.which === 13 && this.newComment.length) {
-        this.addComment();
-      }
-    },  
+    },
     passwordKeydownHandler(event) {
       if (event.which === 13 && this.statePassword) {
         this.changePassword();
@@ -703,18 +314,10 @@ let app = new Vue({
         await this.signIn();
       }
     },
-    async groupKeydownHandler(event) {
-      if (event.which === 13 && this.stateGroup) {
-        await this.createGroup();
-      }
-    },
     async addUserKeydownHandler(event) {
       if (event.which === 13 && this.stateGroup) {
         await this.addUser();
       }
-    },
-    layerVariant(layerID) {
-      return this.draft === layerID ? "light" : "secondary";
     }
   }
 });
